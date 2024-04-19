0f1c8a4f8e18b0b9e6b5015db004dbbbf33d3a2f
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-04-19 11:12:44 +0300

    Initial Commit

---
+++ App482/App482App.swift
@@ -0,0 +1,17 @@
+//
+//  App482App.swift
+//  App482
+//
+//  Created by IGOR on 19/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct App482App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
